6bf37aaed3318765c4e9434139a4694af4ca9ebd
Fraz Ahmad <frazahmad@Frazs-MacBook-Pro-2.local>
2017-11-19 14:41:45 +0000

    Staff Home: Added page flow to staff pages
    plus changed the spelling of listings

--- src/screensframework/StaffHomeController.java
+++ src/screensframework/StaffHomeController.java
@@ -3,6 +3,7 @@ package screensframework;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,13 +16,16 @@ public class StaffHomeController implements Initializable, ControlledScreen {
     private Button btnAddFilm; //go to add film PAGE
     
     @FXML
-    private Button btnAddListening; //go to add film LISTINGS
+    private Button btnAddListings; //go to add film LISTINGS
     
     @FXML
     private Button btnBookingSummary; //go to BOOKING SUMMARY
     
     @FXML
     private Button btnExportFilms; //go to EXPORT FILMS
+    
+    
+    
     
     /**
 	 * Initialises the controller class.
@@ -38,5 +42,33 @@ public class StaffHomeController implements Initializable, ControlledScreen {
 		myController = screenParent;
 	}
 	
+	
+	//METHODS
+	//go to add film 
+	@FXML
+    private void goToAddFilmPage(ActionEvent event){
+       myController.setScreen(ScreensFramework.addFilmPageID);
+    }
+	
+	//go to add listings
+	@FXML
+    private void goToAddListings(ActionEvent event){
+       myController.setScreen(ScreensFramework.addFilmListingsID);
+    }
+	
+	//go to booking summary
+	
+	@FXML
+    private void goToBookingSummary(ActionEvent event){
+       myController.setScreen(ScreensFramework.bookingSummaryID);
+    }
+	
+	//go to export films
+	@FXML
+    private void goToStaffExport(ActionEvent event){
+       myController.setScreen(ScreensFramework.staffExportID);
+    }
+
+		
 	
 }
