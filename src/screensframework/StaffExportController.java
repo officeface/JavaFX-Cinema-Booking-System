16f6aaa6d7a709eacfd11e9649884017e5fce1b9
Fraz Ahmad <frazahmad@Frazs-MacBook-Pro-2.local>
2017-12-04 14:43:37 +0000

    Export feature completed - goes into testofexport1.txt

--- src/screensframework/StaffExportController.java
+++ src/screensframework/StaffExportController.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -97,19 +98,84 @@ public class StaffExportController implements Initializable, ControlledScreen {
 	public void getDatabaseToCSV(ActionEvent event) throws IOException {
 		
 		JSONObject obj = JSONUtils.getJSONObjectFromFile(TextFileManager.database);
-		
 		//Creating a new csv file with FilmList from database at the start
 		String x = (CDL.toString(new JSONArray(obj.get("FilmList").toString())));
 		FileUtils.writeStringToFile(new File("testofexport1.text"), x, "UTF-8");	
-		
 		//Appending FilmTimes to the file
 		String y = (CDL.toString(new JSONArray(obj.get("FilmTimes").toString())));
 		Files.write(Paths.get("testofexport1.text"), y.getBytes(), StandardOpenOption.APPEND);
 
+	
+		//New json array to hold title,free and bookseats info
+		JSONObject obj2 = new JSONObject();
+		JSONArray seatsarry = new JSONArray();
+		
+		JSONObject item = new JSONObject();
+		item.put("title", "");
+		item.put("FreeSeats", "");
+		item.put("BookedSeats", "");
+		item.put("date", "");
+		item.put("time", "");
+		seatsarry.put(item);
+
+		obj2.put("SeatsInfo", seatsarry);
+
+		
+	
+		//For Title values
+		JSONArray jsonArray = obj.getJSONArray("FilmTimes");
+		
+		for (int z = 0; z < jsonArray.length(); z++) {
+			//Get title 
+			String[] temptitleArray = new String[1];
+			temptitleArray[0] = jsonArray.getJSONObject(z).getString("title");
+			
+			//Get date
+			String[] tempdateArray = new String[1];
+			tempdateArray[0] = jsonArray.getJSONObject(z).getString("date");
+			
+			
+			//Get time
+			String[] temptimeArray = new String[1];
+			temptimeArray[0] = jsonArray.getJSONObject(z).getString("time");
+		
+			//Getting seats values for each title
+			String title = temptitleArray[0];
+			Integer getseatinfo = z + 1;
+			String[][] seats = TextFileManager.getSeatInformation(getseatinfo.toString());
+			Integer bookedseatscounter = 0;
+			Integer freeseatscounter = 0;
+				// Generate the seats according to the listing information:
+				for (int i = 0; i < 6; i++) {
+					for (int j = 0; j < 10; j++) {
+						// Check if seat is available:
+						if (seats[i][j].equals("Free")) {
+							freeseatscounter = freeseatscounter + 1;
+						} else {
+							bookedseatscounter = bookedseatscounter + 1;
+						}
+					}
+				};
+			//Setting the labels to their respective numbers  
+			String bsc = bookedseatscounter.toString();
+			String fsc = freeseatscounter.toString();
+			
+			JSONObject templist = new JSONObject();
+			templist.put("title", temptitleArray[0]);
+			templist.put("FreeSeats", fsc);
+			templist.put("BookedSeats", bsc);
+			templist.put("date", tempdateArray[0]);
+			templist.put("time", temptimeArray[0]);
+			seatsarry.put(templist);
+			
+		}//End of loop
+	
+		String z = (CDL.toString(new JSONArray(obj2.get("SeatsInfo").toString())));
+		Files.write(Paths.get("testofexport1.text"), z.getBytes(), StandardOpenOption.APPEND);
 		
 		System.out.println("Database has been exported!");
 		
-	}
+	}//End of overall method
 	
 	
 	     
