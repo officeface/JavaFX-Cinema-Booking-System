52f69dbc5b7ede51dd952141d78f90d15b759d9b
Fraz Ahmad <frazahmad@eduroam-int-dhcp-97-22-74.ucl.ac.uk>
2017-12-13 19:02:20 +0000

    mac image path added

--- src/screensframework/CustBookFilmPageController.java
+++ src/screensframework/CustBookFilmPageController.java
@@ -99,7 +99,7 @@ public class CustBookFilmPageController implements Initializable, ControlledScre
 
 			String imagePath = getImage(CustHomeController.BOOKING.getMovie().getTitle());
 			File file = new File(imagePath);
-			if (imagePath.startsWith("/")) { // One of the built-in images, does not have an absolute filepath listed
+			if (!imagePath.startsWith("/Users/") && imagePath.startsWith("/")) { // One of the built-in images, does not have an absolute filepath listed
 				Image image = new Image(getClass().getResourceAsStream(imagePath));
 				this.imgShowFilmImage.setImage(image);
 			} else { // image has been added by Employee
